2b23cec7ab5c7bcdf4c6058af4b7828111fab2d4
sseosse <sseosse430@gmail.com>
2021-09-29 14:26:50 +0900

    pandas

---
+++ pandas/tsv_to_xlsx.py
@@ -0,0 +1,9 @@
+import glob, os
+import pandas as pd
+import openpyxl
+
+for file in glob.glob("/home/oem/Desktop/tsv/*.tsv"):
+    tsv_name=os.path.basename(file)
+    tsv_name_spt=tsv_name.split(".")[0]
+    tsv = pd.read_csv(f"{tsv_name}",sep='\t',header=None)
+    tsv.to_excel(f"{tsv_name_spt}.xlsx",index=0,header=None)
