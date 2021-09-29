77e6c8b12f647442f4bd8f9a60e79df72352284a
sseosse <sseosse430@gmail.com>
2021-09-29 17:23:48 +0900

    docs: Adding python scripts

---
+++ NGS/WGS_v1.1.py
@@ -0,0 +1,186 @@
+
+import glob, os, sys                                                                           
+import threading, time
+###############################################################################Variable table
+#Directory list
+dir_list = ['01.Trimmomatic','02.BAM','03.GATK','04.ReadCount','05.VCF','06.VEP']
+#Main path
+main_path  = "/data/WES/"
+#Tools path
+tool_path = "/home/oem/Program/"
+#Path of Sample analysis
+sample_path = main_path+"Sample_analysis/"
+#Path of Sample raw data
+srd_path = main_path+"Sample_rawdata/"
+#Threads
+thread = "16"
+#Ram
+ram = "20G"
+#############################################################################################
+
+#Making directory 
+def Mkdir(file):
+    os.chdir(sample_path)
+    if sample_name_spl not in os.getcwd():
+        for dir_name in dir_list:
+            os.system("mkdir -p "+sample_name_spl+"/"+dir_name)
+
+#Raw data trimming
+def Trimmomatic(file):
+    os.chdir(sample_path+sample_name_spl+"/"+dir_list[0])
+    id_right = sample_name.replace("_1.", "_2.")
+    id_log = sample_name.replace("_1.fastq.gz", ".log")
+    id_left_output_paired = sample_name.replace("_1.fastq.gz", "_1.paired.fq")
+    id_right_output_paired = id_left_output_paired.replace("_1", "_2")
+    id_left_output_unpaired = id_left_output_paired.replace("paired", "unpaired")
+    id_right_output_unpaired = id_right_output_paired.replace("paired", "unpaired")
+    if not id_log in os.listdir(os.curdir):
+        cmd = f'java -jar {tool_path}trimmomatic-0.39/trimmomatic-0.39.jar PE -threads {thread} -phred33 {srd_path}{sample_name} {srd_path}{id_right} {id_left_output_paired} {id_left_output_unpaired} {id_right_output_paired} {id_right_output_unpaired} ILLUMINACLIP:{tool_path}trimmomatic-0.39/adapters/TruSeq3-PE.fa:2:40:10:2:KeepBothReads LEADING:3 TRAILING:3 MINLEN:50 2> {id_log}'
+        print(cmd)
+        os.system(cmd)
+
+#Alignment
+def BWA(file):
+    os.chdir(sample_path)
+    sample_dirs = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
+    for sample_dir in sample_dirs:
+        os.chdir(sample_path+sample_dir+"/"+dir_list[0])
+        for id_left_paired in glob.glob("*_1.paired.fq"):
+                id_right_paired = id_left_paired.replace("_1", "_2")
+                id_sam = id_right_paired.split("_2.paired")[0]+".sam"
+                if id_sam not in os.listdir(sample_path+sample_dir+"/"+dir_list[1]):
+                    cmd = f"{tool_path}bwa-0.7.17/bwa mem -t {thread} -k 45 /data/hg38/Homo_sapiens_assembly38.fasta {id_left_paired} {id_right_paired} > {sample_path+sample_dir+'/'+dir_list[1]+'/'+id_sam}"
+                    print(cmd)
+                    os.system(cmd)
+
+#Sam to sorted bam
+def BAM(file):
+    os.chdir(sample_path)
+    sample_dirs = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
+    for sample_dir in sample_dirs:
+        os.chdir(sample_path+sample_dir+"/"+dir_list[1])
+        for id_sam in glob.glob("*.sam"):
+            id_sorted_bam = id_sam.replace(".sam",".sorted.bam")
+            if id_sorted_bam not in os.listdir(os.curdir):
+                cmd = f"{tool_path}samtools-1.12/samtools view -bS {id_sam} | {tool_path}samtools-1.12/samtools sort -@ {thread} -O bam -o {id_sorted_bam}"
+                print(cmd)
+                os.system(cmd)
+
+#Add readgroup
+def RG(file):
+    os.chdir(sample_path)
+    sample_dirs = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
+    for sample_dir in sample_dirs:
+        os.chdir(sample_path+sample_dir+"/"+dir_list[1])
+        for id_bam in glob.glob("*sorted.bam"):
+            RG_bam = id_bam.replace(".bam",".RG.bam")
+            RG_log = id_bam.replace(".bam",".RG.log")
+            if RG_bam not in os.listdir(os.curdir):
+                cmd = f"{tool_path}gatk-4.1.8.1/gatk --java-options '-Xmx{ram} -XX:+UseParallelGC -XX:ParallelGCThreads={thread}' AddOrReplaceReadGroups --SORT_ORDER coordinate --INPUT {id_bam} --OUTPUT {RG_bam} --RGID SH --RGLB WES --RGPL illumina --RGPU OV --RGSM {id_bam.split('-WES')[0]} 2> {RG_log}"
+                print(cmd)
+                os.system(cmd)
+
+#Mark duplicates
+def MarkDuplicates(file):
+    os.chdir(sample_path)
+    sample_dirs = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
+    for sample_dir in sample_dirs:
+        os.chdir(sample_path+sample_dir+"/"+dir_list[1])
+        for RG_bam in glob.glob("*.RG.bam"):
+            marked_bam = RG_bam.replace(".RG.bam",".RG.dedup.bam")
+            marked_log = RG_bam.replace(".RG.bam",".RG.dedup.log")
+            marked_mtr = RG_bam.replace(".RG.bam",".RG.marked.dup.metrics.txt")
+            if marked_bam not in os.listdir(sample_path+sample_dir+"/"+dir_list[2]):
+                cmd = f"{tool_path}gatk-4.1.8.1/gatk --java-options '-Xmx{ram} -XX:+UseParallelGC -XX:ParallelGCThreads={thread}' MarkDuplicates -I {RG_bam} -O ../{dir_list[2]}/{marked_bam} -M ../{dir_list[2]}/{marked_mtr} --VALIDATION_STRINGENCY SILENT --ASSUME_SORTED true --REMOVE_DUPLICATES 2> ../{dir_list[2]}/{marked_log}"
+                print(cmd)
+                os.system(cmd)
+
+#Must make VCF indexing
+#Indexing example
+#/home/oem/Program/gatk-4.1.8.1/gatk --java-options '-Xmx4G -XX:+UseParallelGC -XX:ParallelGCThreads=4' IndexFeatureFile --input resources_broad_hg38_v0_1000G_phase1.snps.high_confidence.hg38.vcf.gz
+
+#Base recalibration + apply
+def BQSR(file):
+    os.chdir(sample_path)
+    sample_dirs = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
+    for sample_dir in sample_dirs:
+        os.chdir(sample_path+sample_dir+"/"+dir_list[2])
+        for marked_bam in glob.glob("*RG.dedup.bam"):
+            bqsr_table = marked_bam.replace("dedup.bam","dedup.recal_data.table")
+            bqsr_log = marked_bam.replace("dedup.bam","dedup.bqsr.log")
+            if bqsr_log not in os.listdir(os.curdir):
+                cmd1 = f"{tool_path}gatk-4.1.8.1/gatk --java-options '-Xmx{ram} -XX:+UseParallelGC -XX:ParallelGCThreads={thread}' BaseRecalibrator -I {marked_bam} -R /data/hg38/Homo_sapiens_assembly38.fasta --known-sites /data/hg38/1000G_phase1.snps.high_confidence.hg38.vcf.gz --known-sites /data/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz -O {bqsr_table} 2> {bqsr_log}"
+                print(cmd1)
+                os.system(cmd1)
+			    
+                bqsr_bam = marked_bam.replace("RG.dedup.bam","RG.dedup.recal.bam")
+                applybqsr_log = bqsr_log.replace("bqsr.log","applybqsr.log")
+                cmd2 = f"{tool_path}gatk-4.1.8.1/gatk --java-options '-Xmx{ram} -XX:+UseParallelGC -XX:ParallelGCThreads={thread}' ApplyBQSR -I {marked_bam} -O {bqsr_bam} --bqsr-recal-file {bqsr_table} 2> {applybqsr_log}"
+                print(cmd2)
+                os.system(cmd2)
+
+#Variant calling
+def Mutect(file):
+    os.chdir(sample_path)
+    sample_dirs = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
+    for sample_dir in sample_dirs:
+        if "-T" in sample_dir:
+            os.chdir(sample_path+sample_dir+"/"+dir_list[2])
+            for recal_bam in glob.glob("*.recal.bam"):
+                tumor_name = recal_bam.split("-WES")[0]
+                norm_name = tumor_name.split("-")[0]+"-N"
+                norm_recal_bam = recal_bam.replace(tumor_name,norm_name)
+                vc_log = tumor_name+".mutect2.log"
+                os.chdir(sample_path+sample_dir+"/"+dir_list[4])
+                if vc_log not in os.listdir(os.curdir):
+                    cmd1 = f"{tool_path}gatk-4.1.8.1/gatk --java-options '-Xmx{ram} -XX:+UseParallelGC -XX:ParallelGCThreads={thread}' Mutect2 -R /data/hg38/Homo_sapiens_assembly38.fasta -I ../{dir_list[2]}/{recal_bam} -tumor {tumor_name} -I {sample_path}{norm_name}/{dir_list[2]}/{norm_recal_bam} -normal {norm_name} -pon /data/hg38/somatic-hg38_1000g_pon.hg38.vcf.gz --germline-resource /data/hg38/somatic-hg38_af-only-gnomad.hg38.vcf.gz --ignore-itr-artifacts --af-of-alleles-not-in-resource 0.0000025 --disable-read-filter MateOnSameContigOrNoMappedMateReadFilter --bam-output {tumor_name}.mutect2.bam -O {tumor_name}.mutect2.unfiltered.vcf 2> {vc_log}"
+                    print(cmd1)
+                    os.system(cmd1)
+
+                    #Filter mutect call
+                    uf_file = tumor_name+".mutect2.unfiltered.vcf"
+                    fmc_log = tumor_name+"_filtermutectcalls.log"
+                    f_file = uf_file.replace(".unfiltered","")
+                    cmd2 = f"{tool_path}gatk-4.1.8.1/gatk --java-options '-Xmx{ram} -XX:+UseParallelGC -XX:ParallelGCThreads={thread}' FilterMutectCalls --max-alt-allele-count 5 -R /data/hg38/Homo_sapiens_assembly38.fasta -V {uf_file} -O {f_file} 2> {fmc_log}"
+                    print(cmd2)
+                    os.system(cmd2)
+
+                    #"PASS" extraction
+                    pass_file = f_file.replace(".vcf",".passed.vcf")
+                    cmd3 = f"/home/oem/Program/bcftools-1.10.2/bin/bcftools view -f 'PASS' {f_file} > {pass_file}"
+                    print(cmd3)
+                    os.system(cmd3)
+
+		
+def VEP(file):
+    os.chdir(sample_path)
+    sample_dirs = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
+    for sample_dir in sample_dirs:
+        os.chdir(sample_path+sample_dir+"/"+dir_list[4])
+        for passed_vcf in glob.glob("*.passed.vcf"):
+            vep_vcf = passed_vcf.replace(".vcf",".VEP.vcf")
+            if vep_vcf not in os.listdir(sample_path+sample_dir+"/"+dir_list[5]):
+                cmd = f"{tool_path}ensembl-vep/vep --force_overwrite --cache --format vcf --vcf -i {passed_vcf} -o ../{dir_list[5]}/{vep_vcf} --fork 32 --symbol --terms SO --tsl --hgvs --pick --refseq REFSEQ_MATCH --use_given_ref --plugin Downstream --plugin Wildtype --plugin Frameshift --fasta /data/hg38/Homo_sapiens_assembly38.fasta --dir_plugins /home/oem/.vep/Plugins"
+                print(cmd)
+                os.system(cmd)
+
+
+
+
+def Main(file):
+    Mkdir(file)
+    Trimmomatic(file)
+    BWA(file)
+    BAM(file)
+    RG(file)
+    MarkDuplicates(file)
+    BQSR(file)
+    Mutect(file)
+    VEP(file)
+
+
+for file in glob.glob(f"{srd_path}*_1.fastq.gz"):
+    sample_name = os.path.basename(file)
+    sample_name_spl = sample_name.split("-WES")[0]
+    Main(file)
+
---
+++ NGS/WTS_v1.1.py
@@ -0,0 +1,100 @@
+import glob, os, sys
+import threading, time
+
+##########################################################################Variable table
+#Directoty list
+dir_list = ['01.Trimmomatic','02.Align','03.Cufflinks','04.HLAtyping','05.ReadCount']
+#Main path
+main_path  = "/data/WTS/"
+#Tools path
+tool_path = "/home/oem/Program/"
+#Path of Sample analysis
+sample_path = main_path+"Sample_analysis/"
+#Path of Sample raw data
+srd_path = main_path+"Sample_rawdata/"
+#Threads
+thread = "16"
+########################################################################################
+
+#Making directory 
+def Mkdir(file):
+    os.chdir(sample_path)
+    if sample_name_spl not in os.getcwd():
+        for dir_name in dir_list:
+            os.system("mkdir -p "+sample_name_spl+"/"+dir_name)
+
+#Trimming raw data
+def Trimmomatic(file):
+    os.chdir(sample_path+sample_name_spl+"/"+dir_list[0])
+    id_right = sample_name.replace("_1.", "_2.")
+    id_log = sample_name.replace("_1.fastq.gz", ".log")
+    id_left_output_paired = sample_name.replace("_1.fastq.gz", "_1.paired.fq")
+    id_right_output_paired = id_left_output_paired.replace("_1", "_2")
+    id_left_output_unpaired = id_left_output_paired.replace("paired", "unpaired")
+    id_right_output_unpaired = id_right_output_paired.replace("paired", "unpaired")
+    if not id_log in os.listdir(os.curdir):
+        cmd = f'java -jar {tool_path}trimmomatic-0.39/trimmomatic-0.39.jar PE -threads {thread} -phred33 {srd_path}{sample_name} {srd_path}{id_right} {id_left_output_paired} {id_left_output_unpaired} {id_right_output_paired} {id_right_output_unpaired} AVGQUAL:20 LEADING:3 TRAILING:3 MINLEN:50 SLIDINGWINDOW:4:15 2> {id_log}'
+        print(cmd)
+        os.system(cmd)
+
+#Aligning trimmed data
+def Align(file):
+	os.chdir(sample_path+sample_name_spl+"/"+dir_list[0])
+	R1_file = sample_name_spl+"-RNA_1.paired.fq"
+	R2_file = R1_file.replace("_1","_2")
+	if not "align_summary.txt" in os.listdir(sample_path+sample_name_spl+"/"+dir_list[1]):
+		cmd = f'{tool_path}tophat-2.1.1.Linux_x86_64/tophat --output-dir ../02.Align --num-threads {thread} --mate-std-dev 56 --library-type fr-unstranded /data/hg38/Homo_sapiens/NCBI/GRCh38/Sequence/Bowtie2Index/genome {R1_file} {R2_file}'
+		print(cmd)
+		os.system(cmd)
+
+#Estimating expression
+def Cufflinks(file):
+	os.chdir(sample_path+sample_name_spl+"/"+dir_list[1])
+	if not f"{sample_name_spl}_Cufflinks.log" in os.listdir(sample_path+sample_name_spl+"/"+dir_list[2]):
+		cmd = f'{tool_path}cufflinks-2.2.1.Linux_x86_64/cufflinks --output-dir ../{dir_list[2]} --num-threads {thread} --library-type fr-unstranded --GTF /data/hg38/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.gtf --multi-read-correct --max-mle-iterations 10000 --max-bundle-frags 5000000 --compatible-hits-norm accepted_hits.bam 2> ../{dir_list[2]}/{sample_name_spl}_Cufflinks.log'
+		print(cmd)
+		os.system(cmd)
+
+#HLA typing
+def HLAminer(file):
+    os.chdir(main_path+'HLAminer_v1.4/data')
+    if not "HLAminer_HPTASR.log" in os.listdir(sample_path+sample_name_spl+"/"+dir_list[3]):
+        sample_name2 = sample_name.replace("_1.", "_2.")
+        #Making link raw data
+        cmd = f'ln -s {srd_path}{sample_name} .'
+        cmd2 = f'ln -s {srd_path}{sample_name2} .'
+        print(cmd)
+        os.system(cmd)
+        print(cmd2)
+        os.system(cmd2)
+        
+        #Writing raw data name
+        cmd3 = f'echo {sample_name} > patient.fof'
+        cmd4 = f'echo {sample_name2} >> patient.fof'
+        print(cmd3)
+        os.system(cmd3)
+        print(cmd4)
+        os.system(cmd4)
+        
+        #Running HLAminer
+        cmd5 = './HPTASRrnaseq_classI-II.sh'
+        print(cmd5)
+        os.system(cmd5)
+        
+        #Making copy result
+        cmd6 = f'cp *_HPTASR.* {sample_path}{sample_name_spl}/{dir_list[3]}'
+        print(cmd6)
+        os.system(cmd6)
+
+def Main(file):
+	Mkdir(file)
+	Trimmomatic(file)
+	Align(file)
+	Cufflinks(file)
+	HLAminer(file)
+
+for file in glob.glob(f"{srd_path}*_1.fastq.gz"):
+    sample_name = os.path.basename(file)
+    sample_name_spl = sample_name.split("-RNA")[0]
+    Main(file)
+
