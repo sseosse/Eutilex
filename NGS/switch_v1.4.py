ee7f3586ab4a0de1d984b0285b021740d5130ca0
sseosse <sseosse430@gmail.com>
2021-09-30 09:24:48 +0900

    docs : Adding description

--- NGS/switch_v1.4.py
+++ NGS/switch_v1.4.py
@@ -1,4 +1,6 @@
 #!/bin/python
-import os
 
+import os
+# 1.Running WES, WTS, HLA typing at the same time.
+# 2.Running pVACseq when step 1 is finished. 
 os.system("(python WES/WES_v1.4.py & python WTS/WTS_v1.4.py & python WTS/HLAtyping_v1.1.py) && (conda run -n Neoang python pVACseq/pVACseq_v1.7_lenient.py & conda run -n Neoang python pVACseq/pVACseq_v1.7_strict.py)")
